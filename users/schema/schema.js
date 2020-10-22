dbe47654df1dc29b23754194b6e48847a469de1c
Erik Soderholm <esodey@gmail.com>
2020-10-22 15:09:10 -0700

    "Create root schema and connect it to server using mock data"

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,12 +1,38 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
+
+const users = [
+  {id: '23', firstName: 'Bill', age: 20 },
+  {id: '47', firstName: 'Samantha', age: 21 }
+]
 
 const Usertype = new GraphQLObjectType({
     name: 'User',
     fields: {
       id: { type: GraphQLString },
       firstName: { type: GraphQLString },
-      age: { type: graphql.GraphQLInt } 
+      age: { type: GraphQLInt } 
     }
+})
+
+
+const RootQuery = new graphql.GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: Usertype,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id})
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
